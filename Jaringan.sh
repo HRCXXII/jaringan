4d8aaac6074f916b8cd3c681d340766ddf98734f
MR.HRCXXII <betazona1984@gmail.com>
2022-12-23 23:44:02 +0900

    Add files via upload

---
+++ Jaringan.sh
@@ -0,0 +1,52 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay HRCXXII
+echo $red"SELAMAT DATANG BOSKU"
+echo
+sleep 2
+echo $yellow"Silahkan Memilih"
+echo
+echo $blue"1.) Stabilkan Jaringan"
+echo
+echo $blue"2.) Cek Ip Addres"
+echo
+echo $green"3.) Keluar"
+echo
+echo$white
+read -p "PILIHANMU : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay HRCXXII
+echo $yellow"stabilkan jaringan by CyberXXII"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f big -F gay HRCXXII
+echo $yellow"cek ip adress by CyberXXII"
+sleep 3
+ifconfig
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+echo $yellow
+figlet "Bye-Bye"
+echo $yellow"To Be Continue"
+sleep 4
+exit
+fi
